5c6ea3311f97e776218f02b9d5eee9b1a6de23dd
Adolfo Jara <gavilanesadolfo@gmail.com>
2020-05-25 07:14:56 -0500

    Creacion del layout del frame principal y del panel registrar que contiene todos los campos para crear un nuevo telefono del usuario.

--- src/ec/ups/edu/test/Test.java
+++ src/ec/ups/edu/test/Test.java
@@ -31,6 +31,56 @@ public class Test {
         Panel registrar = new Panel();
         Panel panelSur = new Panel();
         BorderLayout posiciones = new BorderLayout();
+        
+        GridLayout mallaP = new GridLayout(7, 1);
+        principal.setLayout(mallaP);
+
+        Label mensaje = new Label("Bienvenido");
+        panelNorte.add(mensaje);
+
+        principal.add(panelNorte);
+
+        GridLayout malla = new GridLayout(5, 1);
+        panelPrincipal.setLayout(malla);
+        panelCentro.setLayout(malla);
+
+        GridLayout mallaR = new GridLayout(4, 2, 0, 0);
+        registrar.setLayout(mallaR);
+
+        panelRegistrar.setLayout(posiciones);
+
+        Label registrarT = new Label("Registrar un nuevo Telefono");
+        Label numeroR = new Label("Numero del telefono:\n");
+        TextField numero = new TextField("0987456123", 10);
+        Label tipoR = new Label("Tipo de telefono:\n");
+        TextField tipo = new TextField("celular", 10);
+        Label operadoraR = new Label("Operadora del telefono:\n");
+        TextField operadora = new TextField("movistar", 10);
+        Label codigoR = new Label("Codigo:\n");
+        TextField codigo = new TextField("1", 5);
+        Button crearR = new Button("Crear Telefono");
+
+        panelRegistrar.add(registrarT, BorderLayout.NORTH);
+        registrar.add(numeroR);
+        registrar.add(numero);
+        registrar.add(tipoR);
+        registrar.add(tipo);
+        registrar.add(operadoraR);
+        registrar.add(operadora);
+        registrar.add(codigoR);
+        registrar.add(codigo);
+        panelRegistrar.add(crearR, BorderLayout.SOUTH);
+        panelRegistrar.add(registrar, BorderLayout.CENTER);
+
+       
+
+       // panelCentro.add(panelRegistrar);
+
+         principal.add(panelRegistrar);
+        
+         
+         
+        
 
        
         
